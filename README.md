b8954f6b9cbb1203928e26867ddb8f557aa74f6d
Lucas <lucas97lopes@hotmail.com>
2021-01-24 16:13:19 -0300

    finish

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Aula com Bonieki Lacerda
-No youtube - https://www.youtube.com/watch?v=tBweoUiMsDg&t=2480s
-- 1 Parte da aula parei em - 1:20:00
+# Apenas um estudo de ReactJS
--- src/components/FeaturedMovie/index.jsx
+++ src/components/FeaturedMovie/index.jsx
@@ -3,8 +3,8 @@ import './style.css';
 export default function MovieRow({item}) {
     return (
         <div className="movieRow--banner-principal">
-            <div src={`https://image.tmdb.org/t/p/original${item.backdrop_path}`} alt={item.original_name} />
-            <h1>{item.name}</h1>
+            <img src={`https://image.tmdb.org/t/p/original${item.backdrop_path}`} alt={item.original_name} />
+            <h1>{item.original_name}</h1>
             <h3>{item.overview}</h3>
         </div>
     )
